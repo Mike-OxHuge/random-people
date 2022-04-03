2157e3d4a83e555d65c047898b0d6dd7eefba233
Mike-OxHuge <mikelitoris34@icloud.com>
2022-04-03 15:27:21 +0200

    init

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.printMsg = function () {
 	console.log("This is a message from the demo package");
 };
+
+// generates an array of random people: name, surname, occupation, photo, age and location.
