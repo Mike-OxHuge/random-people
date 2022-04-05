baed3def7b4037fab84298e25d496e40df6a43ca
Mike-OxHuge <mikelitoris34@icloud.com>
2022-04-05 16:37:58 +0200

    core functionality

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
-exports.printMsg = function () {
-	console.log("This is a message from the demo package");
+import maleNames from "./lib/maleNames.js";
+import femaleNames from "./lib/femaleNames.js";
+import surnames from "./lib/surnames.js";
+
+const allowedSex = ["f", "m", "rnd"];
+
+const rnd = (max) => {
+	const num = Math.floor(Math.random() * (max - 1));
+	return num;
+};
+const getRandomName = (gender) => {
+	gender = gender.toLowerCase();
+	if (gender === "f") return femaleNames[rnd(femaleNames.length)];
+	if (gender === "m") return maleNames[rnd(maleNames.length)];
+	return rnd(3) === 1
+		? maleNames[rnd(maleNames.length)]
+		: femaleNames[rnd(femaleNames.length)];
 };
 
-// generates an array of random people: name, surname, occupation, photo, age and location.
+const isNumber = (n) => !isNaN(parseFloat(n)) && isFinite(n);
+const isValidSex = (s) => allowedSex.includes(s.toLowerCase());
+
+export default function (params) {
+	const { amount, gender, withPhoto } = params;
+	const users = [];
+	try {
+		if (!isNumber(amount)) throw Error("amount must be a number!");
+		if (!isValidSex(gender))
+			throw Error("gender must be 'f' or 'm' or 'rnd'");
+
+		for (let i = 0; i < amount; i++) {
+			let user = {};
+			user.firstName = getRandomName(gender);
+			user.lastName = surnames[rnd(surnames.length)];
+			user.username = user.firstName[0] + "." + user.lastName;
+			// figure this thing out
+			if (withPhoto)
+				user.photo = `https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}`;
+			users.push(user);
+		}
+	} catch (e) {
+		users.push({
+			firstName: "Error occured",
+			lastName: "Please check console",
+			username: "Error occured",
+			photo: `https://ui-avatars.com/api/?name=Error+Occured}`,
+		});
+		console.error(e.message);
+	}
+	return users;
+}
+
+/*
+users = [
+	{
+		firstName: 'generated',
+		lastName: 'generated',
+		username: GENERATE
+		dateOfBirth: GENERATE,
+		age: CALCULATE,
+		photo: GENERATE??,
+	}
+]
+*/
--- readme.md
+++ readme.md
@@ -1,1 +1,58 @@
-test init
+Basic usage:
+
+`npm i randomized-people`
+
+```
+import people from "randomized-people"; // import package
+
+// set options
+const options = {
+	amount: 5, // always number, or string convertible to number
+	gender: "rnd", // 'm' or 'f' or 'rnd', case insensitive
+	withPhoto: true, // true or false
+};
+
+// declare variable and assign value to it
+const users = people(options);
+
+// use the array
+console.log(users);
+```
+
+### expected output:
+
+```
+[
+  {
+    firstName: 'Angela',
+    lastName: 'Rivera',
+    username: 'A.Rivera',
+    photo: 'https://ui-avatars.com/api/?name=Angela+Rivera'
+  },
+  {
+    firstName: 'Robert',
+    lastName: 'Murphy',
+    username: 'R.Murphy',
+    photo: 'https://ui-avatars.com/api/?name=Robert+Murphy'
+  },
+  {
+    firstName: 'Anna',
+    lastName: 'Nguyen',
+    username: 'A.Nguyen',
+    photo: 'https://ui-avatars.com/api/?name=Anna+Nguyen'
+  },
+  {
+    firstName: 'Catherine',
+    lastName: 'Baker',
+    username: 'C.Baker',
+    photo: 'https://ui-avatars.com/api/?name=Catherine+Baker'
+  },
+  {
+    firstName: 'Gregory',
+    lastName: 'Howard',
+    username: 'G.Howard',
+    photo: 'https://ui-avatars.com/api/?name=Gregory+Howard'
+  }
+]
+
+```
